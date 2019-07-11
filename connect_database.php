438fbde64e191ee1dc0abcef58aa9486a39bb173
jibanbarman <48866661+jibanbarman@users.noreply.github.com>
2019-07-11 20:39:53 +0530

    9th

---
+++ connect_database.php
@@ -0,0 +1,20 @@
+<?php
+$con = mysqli_connect('localhost','root','');
+ 
+
+if ( !$con)
+{
+	echo 'Not connected to server';
+}  
+else{
+	//echo "connected to server";
+}
+if (!mysqli_select_db($con,'my_db'))
+{
+	echo 'Database not selected';
+}
+else{
+    echo "database selected";
+}
+
+?>
