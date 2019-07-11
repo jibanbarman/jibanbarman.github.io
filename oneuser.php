1a587002682b2920f8360bf2dfb6d6e6dc12adc9
jibanbarman <48866661+jibanbarman@users.noreply.github.com>
2019-07-11 19:28:08 +0530

    latest commit for website

---
+++ oneuser.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta name="viewport" content="width=device-width,initial-scale=1">
+        <title>Home Page</title>
+<link rel="stylesheet" href="css.css">
+<script src="js.js"></script> 
+        
+        <div id="main_header">
+            <marquee>  <h1>CREDIT MANAGEMENT SYSTEM  </h1></marquee>
+    
+        </div>
+         <nav class="navigation_bar">
+             <div class="menu">  
+			 <a href="index.html">Home </a>&nbsp;&nbsp;|&nbsp;&nbsp;
+             <a href="about.html">About</a>&nbsp;&nbsp;|&nbsp;&nbsp;
+             <a href="contact.html">Contact</a>
+                 </div>
+        </nav>
+            </head>
+    
+    <body >
+        
+        <div class="body_container">
+              <aside>
+        <h1><!Side Bar></h1>    
+    </aside>
+	<table id="outputs">
+	<tr>
+	
+	<th>Ids and Credits </th>
+	
+	</tr>
+        <?php
+include ('connect_database.php');
+
+
+$query = mysqli_query($con,"SELECT * FROM users");
+while($rows = mysqli_fetch_array($query))
+{   $Id=$rows['ID'];
+	
+	$Credit=$rows['Current credit'];
+
+echo "<tr><td>"."$Id" . "</tr></td>" ;
+echo "<tr><td>"."$Credit" . "</tr></td>" ;
+}
+
+?>
+	</table>
+         
+        </div>
+      
+    </body>
+    
+    
+         <footer>
+           <div class="footer_part">  &copy; Credit Management system &nbsp;<span class="separator">|</span> Design by  <a  href="https://www.linkedin.com/in/jiban-barman-bb-153a161">Jiban Barman</a>
+             </div>
+         </footer>     
+         
+</html>
